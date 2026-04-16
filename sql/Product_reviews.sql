35358ce2b980e0e0dcf347cd42656e9db68c5ddc
Conner Erich <cje00016@mix.wvu.edu>
2026-04-16 17:15:23 -0400

    renamed a product_reviews as it was originaly spelled prodcut

---
+++ sql/Product_reviews.sql
@@ -0,0 +1,33 @@
+-- this CTE pairs the review, order and the customer so that it is posible to see what customers think of the product and how much said product was worth
+with product_reviews as(
+select
+  ore.review_id,
+  ore.order_id,
+  c.customer_id,
+  op.payment_value,
+  ore.review_score,
+  case
+    When ore.review_score <= 2 then 'bad'
+    when ore.review_score = 3 then 'okay'
+    when ore.review_score <= 4 then 'great'
+    else 'error'
+  end as review_rating
+from
+  order_reviews as ore
+  join orders as o on ore.order_id = o.order_id
+  join customers as c on c.customer_id = o.customer_id
+  join order_payments as op on op.order_id = o.order_id
+group by
+  review_id,
+  ore.order_id,
+  o.order_id,
+  c.customer_id,
+  o.customer_id,
+  op.order_id,
+  op.payment_value,
+  ore.review_score
+order by
+  ore.review_score asc
+  )
+  select *
+  from product_reviews
