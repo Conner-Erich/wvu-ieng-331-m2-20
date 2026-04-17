878958a607c851992385c9a0660af32edab5aaae
Conner Erich <cje00016@mix.wvu.edu>
2026-04-17 11:41:34 -0400

    updated queries so that it can more easily be used in validation.py by creating variable data_set_path so the data base can easily be changed for the stress test

--- src/wvu_ieng_331_m2_20/queries.py
+++ src/wvu_ieng_331_m2_20/queries.py
@@ -8,10 +8,16 @@ DB_PATH = Path(__file__).parent.parent.parent / "data"
 
 
 def data_path(filename: str) -> Path:
+    """Sets the database file to read and out-puts it as a path"""
     data_set = DB_PATH / filename
     if not data_set.exists():
         raise FileNotFoundError(f"Data set not found {data_set}")
     return Path(__file__).parent.parent.parent / "data" / filename
+
+
+data_set_path = data_path(
+    "olist.duckdb"
+)  # uses the function to set olist.duckdb as the path
 
 
 def read_sql(filename: str) -> str:
@@ -22,26 +28,26 @@ def read_sql(filename: str) -> str:
     return sql_path.read_text(encoding="utf-8")
 
 
-conn_payment_info = duckdb.connect(data_path("olist.duckdb"), read_only=True)
+conn_payment_info = duckdb.connect(data_set_path, read_only=True)
 get_payment_information = conn_payment_info.sql(
     read_sql("Payment_information.sql"), params=[1]
 ).pl()
 
-conn_price_shipping = duckdb.connect(data_path("olist.duckdb"), read_only=True)
+conn_price_shipping = duckdb.connect(data_set_path, read_only=True)
 get_price_shipping = conn_price_shipping.sql(
     read_sql("Price_shipping.sql"), params=["cheap"]
 ).pl()
 
-conn_product_reviews = duckdb.connect(data_path("olist.duckdb"), read_only=True)
+conn_product_reviews = duckdb.connect(data_set_path, read_only=True)
 get_product_reviews = conn_product_reviews.sql(
     read_sql("Product_reviews.sql"), params=["poor"]
 ).pl()
 
-conn_seller_con_loc = duckdb.connect(data_path("olist.duckdb"), read_only=True)
+conn_seller_con_loc = duckdb.connect(data_set_path, read_only=True)
 get_seller_consumer_location = conn_seller_con_loc.sql(
     read_sql("Seller_consumer_location.sql"), params=["alambari"]
 ).pl()
 
 
 if __name__ == "__main__":
-    print(get_seller_consumer_location)
+    print(get_payment_information)  # verifys that data is properly pulled
